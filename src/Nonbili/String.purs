864be47ad5a3d51046a261260f0f391b9bb433ca
Ping Chen <remotenonsense@gmail.com>
2019-05-05 11:07:08 +0900

    Add search and startsWith to Nonbili.String

--- src/Nonbili/String.purs
+++ src/Nonbili/String.purs
@@ -1,10 +1,13 @@
 module Nonbili.String
   ( padStart
+  , search
+  , startsWith
   ) where
 
 import Nonbili.Prelude
 
 import Data.Array as Array
+import Data.String (Pattern(..))
 import Data.String as String
 import Data.String.CodeUnits (fromCharArray)
 
@@ -13,3 +16,14 @@ padStart targetLength input =
   fromCharArray (Array.replicate padLength '0') <> input
   where
   padLength = max (targetLength - String.length input) 0
+
+search :: Pattern -> Array String -> Array String
+search (Pattern term) options =
+  Array.sortWith (\p -> String.indexOf pattern $ String.toLower p) $
+    Array.filter (\p -> String.contains pattern $ String.toLower p) options
+  where
+  pattern = String.Pattern $ String.toLower term
+
+startsWith :: Pattern -> String -> Boolean
+startsWith (Pattern p) str =
+  String.take (String.length p) str == p
--- test/Nonbili/String.purs
+++ test/Nonbili/String.purs
@@ -2,16 +2,17 @@ module Test.Nonbili.String where
 
 import Nonbili.Prelude
 
+import Data.String (Pattern(..))
 import Data.String as String
 import Data.String.Regex as Regex
 import Data.String.Regex.Flags as RegexFlags
 import Data.String.Regex.Unsafe (unsafeRegex)
-import Jest (test)
-import Nonbili.String (padStart)
+import Jest (expectToBeFalse, expectToBeTrue, expectToEqual, test)
+import Nonbili.String (padStart, search, startsWith)
 import Test.QuickCheck (quickCheck)
 
 spec :: Effect Unit
-spec =
+spec = do
   test "padStart" $ do
     let re = unsafeRegex "^0+$" RegexFlags.noFlags
     quickCheck \n s -> do
@@ -21,3 +22,19 @@ spec =
       after == s && if lengthDiff <= 0
         then before == ""
         else Regex.test re before
+
+  test "search" $ do
+    expectToEqual (search (Pattern "re") ["Haskell", "PureScript", "Regex", "Web"])
+      ["Regex", "PureScript"]
+
+    expectToEqual (search (Pattern "") ["Haskell", "PureScript", "Regex", "Web"])
+      ["Haskell", "PureScript", "Regex", "Web"]
+
+    expectToEqual (search (Pattern "abc") ["Haskell", "PureScript", "Regex", "Web"])
+      []
+
+  test "startsWith" $ do
+    expectToBeTrue $ startsWith (Pattern "Pure") "PureScript"
+    expectToBeTrue $ startsWith (Pattern "") "PureScript"
+    expectToBeTrue $ startsWith (Pattern "") ""
+    expectToBeFalse $ startsWith (Pattern "Pure") "Regex"
